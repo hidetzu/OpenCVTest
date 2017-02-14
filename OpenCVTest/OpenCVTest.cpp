079e7c670faef85a47f75e97ecb1a9799a12a6e2
hidetzu <hidetzu@gmail.com>
2017-02-15 06:50:58 +0900

    HSVを利用したクロマキー

--- OpenCVTest/OpenCVTest.cpp
+++ OpenCVTest/OpenCVTest.cpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 #include <thread>
+#include <string.h>
+#include <stdlib.h>
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS 
 
@@ -167,11 +169,114 @@ void encode_decode()
 	WSACleanup();
 }
 
+void sample2()
+{
+	//画像の読み込み
+	cv::Mat img_src1 = cv::imread("sora.jpg", cv::IMREAD_COLOR);
+	cv::Mat gori(cv::Size(img_src1.cols, img_src1.rows), CV_8UC3);
+	int bytes = img_src1.total() * img_src1.elemSize();
+
+	uchar* data = new uchar[bytes];
+	memcpy(data, img_src1.data, bytes);
+
+#if false
+	for (int j = 0; j<height; j++)
+	{
+		int step = j*width;
+		for (int i = 0; i<width; i++)
+		{
+			int elm = i*img_src1.elemSize();
+			for (int c = 0; c<channels; c++)
+			{
+				data[step + elm + c] = img_src1.data[step + elm + c];
+			}
+		}
+	}
+#endif
+	gori.data = data;
+
+	cv::namedWindow("both flip image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+	cv::imshow("both flip image", gori);
+	cv::imshow("画像", img_src1);
+
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+}
+
+void sample3()
+{
+	//画像の読み込み
+	cv::Mat img_src1 = cv::imread("dummy_1.png", cv::IMREAD_COLOR);
+	//cv::Mat img_src2 = cv::imread("sample_bg.jpg", cv::IMREAD_COLOR);
+	cv::Mat img_src2(cv::Size(img_src1.cols, img_src1.rows), CV_8UC3, cv::Scalar::all(255));
+	if (img_src1.empty())
+		return;
+
+	if (img_src2.empty())
+		return;
+
+	cv::Mat hsv;
+	cv::cvtColor(img_src1, hsv, cv::COLOR_BGR2HSV);
+
+	//OpenCV HSV H : 0 - 180, S : 0 - 255, V : 0 - 255
+	//	lower_color = np.array([100, 110, 30]) # 色空間の下限
+	//	upper_color = np.array([120, 255, 255]) # 色空間の上限
+	cv::Mat mask;
+	cv::inRange(hsv,
+		cv::Scalar(40,  0,  30, 0),
+		cv::Scalar(100, 255, 255, 0),
+		mask);
+
+	// ノイズ除去
+#if true
+	cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, cv::Mat(), cv::Point(-1, -1), 1);
+	cv::morphologyEx(mask, mask, cv::MORPH_OPEN,  cv::Mat(), cv::Point(-1, -1), 1);
+#endif
+
+	cv::imwrite("mask.png", mask);
+
+	// 人物画を抽出
+	cv::Mat portraitPhotographTmp;
+	{
+		cv::Mat notImg;
+		cv::bitwise_not(mask, notImg);
+		img_src1.copyTo(portraitPhotographTmp, notImg);
+		cv::imwrite("portraitPhotographTmp.jpg", portraitPhotographTmp);
+	}
+
+	// 背景画を抽出 
+	cv::Mat backgroundPictureTmp;
+	{
+		img_src2.copyTo(backgroundPictureTmp, mask);
+		cv::imwrite("backgroundPictureTmp.jpg", backgroundPictureTmp);
+	}
+
+	// 人物画と背景画を合成する。
+	cv::Mat result;
+	cv::bitwise_or(backgroundPictureTmp, portraitPhotographTmp, result);
+	cv::imshow("Show MASK COMPOSITION Image", result);
+
+#if false
+	cv::Mat notImg;
+	cv::Mat result;
+	cv::bitwise_not(mask, notImg); // マスクを反転
+	cv::imwrite("notmask.png", notImg);
+	cv::bitwise_and(img_src1, img_src1, result, notImg);
+
+	// 結果の表示
+	cv::imshow("Show MASK COMPOSITION Image", result);
+#endif
+
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+}
 
 int main(int argc, const char* argv[])
 {
 //	sample1();
-	encode_decode();
+//	encode_decode();
+//	sample2();
+	sample3();
 
 	return 0;
 }
