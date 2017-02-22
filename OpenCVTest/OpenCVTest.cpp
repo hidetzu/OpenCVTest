a3e4a55c9cc3e468777be29daa7f82317ce80129
hidetzu <hidetzu@gmail.com>
2017-02-23 07:13:36 +0900

    サンプル実装

--- OpenCVTest/OpenCVTest.cpp
+++ OpenCVTest/OpenCVTest.cpp
@@ -42,167 +42,6 @@ void sample1()
 	cv::destroyAllWindows();
 }
 
-void sendThread() {
-	SOCKET s, s1;         //ソケット
-	int result;          //戻り値
-						 //接続を許可するクライアント端末の情報
-	struct sockaddr_in source;
-	char buffer[1024];  //受信データのバッファ領域
-	char ans[] = "送信成功";
-	char ret;
-
-	memset(&buffer, '\0', sizeof(buffer));
-
-	//送信元の端末情報を登録する
-	memset(&source, 0, sizeof(source));
-	source.sin_family = AF_INET;
-
-	//ポート番号はクライアントプログラムと共通
-	source.sin_port = htons(7000);
-	source.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	//ソケットの生成
-	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (s < 0) {
-		printf("%d\n", GetLastError());
-		printf("ソケット生成エラー\n");
-	}
-
-	//ソケットのバインド
-	result = bind(s, (struct sockaddr *)&source, sizeof(source));
-	if (result < 0) {
-		printf("%d\n", GetLastError());
-		printf("バインドエラー\n");
-	}
-
-	//接続の許可
-	result = listen(s, 1);
-	if (result < 0) {
-		printf("接続許可エラー\n");
-	}
-
-	printf("接続開始\n");
-	//クライアントから通信があるまで待機
-	s1 = accept(s, NULL, NULL);
-	if (s1 < 0) {
-		printf("待機エラー\n");
-	}
-
-	cv::Mat frameReference = cv::imread("lena.png", cv::IMREAD_COLOR);
-	std::vector<uchar> buf;
-
-	std::vector<int> params = std::vector<int>(2);
-	params[0] = CV_IMWRITE_PNG_COMPRESSION;
-	params[1] = 9;
-
-	cv::imencode(".png", frameReference, buf, params);
-
-	auto imgSize = buf.size();
-	send(s1, (char*)&imgSize, sizeof(int64_t), 0);
-	send(s1, (char*)(&buf[0]), imgSize, 0);
-
-	printf("接続終了\n");
-	closesocket(s1);
-}
-
-void encode_decode()
-{
-	WSADATA data;
-	WSAStartup(MAKEWORD(2, 0), &data);
-
-	std::thread th(sendThread);
-	SOCKET s;    //ソケット
-				 //接続するサーバの情報
-	struct sockaddr_in dest;
-	char destination[] = "127.0.0.1";
-	char buffer[1024];
-
-	//接続先（サーバ）のアドレス情報を設定
-	memset(&dest, 0, sizeof(dest));
-
-	//ポート番号はサーバプログラムと共通
-	dest.sin_port = htons(7000);
-	dest.sin_family = AF_INET;
-	dest.sin_addr.s_addr = inet_addr(destination);
-
-	//ソケットの生成
-	s = socket(AF_INET, SOCK_STREAM, 0);
-
-	//サーバへの接続
-	if (connect(s, (struct sockaddr *) &dest, sizeof(dest))) {
-		printf("%sに接続できませんでした\n", destination);
-		return;
-	}
-
-	printf("%sに接続しました\n", destination);
-
-	int64_t bufSize;
-	recv(s, (char*)&bufSize, sizeof(int64_t), 0);
-
-	uchar*  buffer2 = new uchar[bufSize];
-
-	//サーバからデータを受信
-	int bytes = 0;
-	for (int i = 0; i < bufSize; i += bytes) {
-		if ((bytes = recv(s, (char*)(buffer2 + i), bufSize - i, 0)) == -1) {
-		}
-	}
-
-	std::vector<uchar> buf;
-
-	for (int i = 0; i < bufSize; i++) {
-		buf.push_back(buffer2[i]);
-	}
-
-	closesocket(s);
-
-	/// デコード(from jpeg200)
-	// バッファ，imreadと同じフラグ
-	cv::Mat dst_img = cv::imdecode(cv::Mat(buf), 1);
-
-	cv::namedWindow("both flip image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-	cv::imshow("both flip image", dst_img);
-	cv::waitKey(0);
-
-	th.join();
-
-	WSACleanup();
-}
-
-void sample2()
-{
-	//画像の読み込み
-	cv::Mat img_src1 = cv::imread("sora.jpg", cv::IMREAD_COLOR);
-	cv::Mat gori(cv::Size(img_src1.cols, img_src1.rows), CV_8UC3);
-	int bytes = img_src1.total() * img_src1.elemSize();
-
-	uchar* data = new uchar[bytes];
-	memcpy(data, img_src1.data, bytes);
-
-#if false
-	for (int j = 0; j<height; j++)
-	{
-		int step = j*width;
-		for (int i = 0; i<width; i++)
-		{
-			int elm = i*img_src1.elemSize();
-			for (int c = 0; c<channels; c++)
-			{
-				data[step + elm + c] = img_src1.data[step + elm + c];
-			}
-		}
-	}
-#endif
-	gori.data = data;
-
-	cv::namedWindow("both flip image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-	cv::imshow("both flip image", gori);
-	cv::imshow("画像", img_src1);
-
-	cv::waitKey(0);
-	cv::destroyAllWindows();
-}
-
 void sample3()
 {
 	//画像の読み込み
@@ -411,13 +250,12 @@ void soft_mask(
 	int     kk;
 	for (int y = 0; y < image.rows; ++y)
 	{
-		cv::Vec3b* imgPtr  = image.ptr<cv::Vec3b>(y);
+		uchar* imgPtr  = image.ptr<uchar>(y);
 		uchar* keyImgPtr = keyImage.ptr<uchar>(y);
 		for (int x = 0; x < image.cols; ++x)
 		{
 			auto imgBgr = imgPtr[x];
-			d = (imgBgr[0] + imgBgr[2]) / 2 - imgBgr[1];
-
+			d = imgBgr;
 			kk = ((long)(d - thdl) * 255 / (thdh - thdl));
 			if (kk > 255)        keyImgPtr[x] = 255;
 			else if (kk < 0)     keyImgPtr[x] = 0;
@@ -478,9 +316,9 @@ void s_synth(cv::Mat& fgImg, cv::Mat& bgImg, cv::Mat& keyImg, cv::Mat* pOutImg)
 			long kk = (long)keyImgPtr[x];
 
 			if (kk == 255 || kk == 0) {       /* 前景または背景 */
-				auto rr = (unsigned char)((rr1*kk + rr2*(255 - kk)) / 255);
-				auto gg = (unsigned char)((gg1*kk + gg2*(255 - kk)) / 255);
-				auto bb = (unsigned char)((bb1*kk + bb2*(255 - kk)) / 255);
+				auto rr = (uchar)((rr1*kk + rr2*(255 - kk)) / 255);
+				auto gg = (uchar)((gg1*kk + gg2*(255 - kk)) / 255);
+				auto bb = (uchar)((bb1*kk + bb2*(255 - kk)) / 255);
 				outImgPtr[x] = cv::Vec3b(bb, gg, rr);
 			}
 			else {                              /* 境界部 */
@@ -493,42 +331,277 @@ void s_synth(cv::Mat& fgImg, cv::Mat& bgImg, cv::Mat& keyImg, cv::Mat* pOutImg)
 	}
 }
 
+void bitwise_andEx(cv::Mat& src1, cv::Mat& src2, cv::Mat* pOutImg)
+{
+}
+
 void sample6()
 {
 	//画像の読み込み
 	cv::Mat fgImg = cv::imread("sample_fg.jpg", cv::IMREAD_COLOR);
 	cv::Mat bgImg = cv::imread("sample_fg_bg.jpg", cv::IMREAD_COLOR);
 	cv::Mat img_back(cv::Size(fgImg.cols, fgImg.rows), CV_8UC3, cv::Scalar::all(255));
-	cv::Mat result = fgImg.clone();
-
-	cv::Mat img_src1_gray;
-	cv::Mat img_src2_gray;
-	cv::cvtColor(fgImg, img_src1_gray, cv::COLOR_BGR2GRAY);
-	cv::cvtColor(bgImg, img_src2_gray, cv::COLOR_BGR2GRAY);
+	cv::Mat result = img_back.clone();
 
 	cv::Mat matDiff;
 	cv::absdiff(fgImg, bgImg, matDiff);
 	cv::cvtColor(matDiff, matDiff, cv::COLOR_BGR2GRAY);
 	cv::imwrite("matDiff.png", matDiff);
 
-	cv::Mat binaryImg = matDiff.clone();
-	soft_mask(matDiff, binaryImg, 1, 10);
-//	cv::threshold(matDiff, binaryImg, 40, 255, CV_THRESH_BINARY_INV);
+	cv::Mat binaryImg(cv::Size(fgImg.cols, fgImg.rows), CV_8UC1, cv::Scalar::all(255));
+//	soft_mask(matDiff, binaryImg, 20, 40);
+//	cv::bitwise_not(binaryImg, binaryImg);
+	cv::threshold(matDiff, binaryImg, 40, 255, CV_THRESH_BINARY_INV);
 	cv::imwrite("binary.png", binaryImg);
 
-	cv::Mat keyImg = cv::Mat(fgImg.rows, fgImg.cols, CV_8UC1, cv::Scalar(255));
+#if false
 	s_synth(fgImg, img_back, binaryImg, &result);
+#else
+	{
+		// 人物画を抽出
+		cv::Mat portraitPhotographTmp;
+		{
+			cv::Mat notImg;
+			cv::bitwise_not(binaryImg, notImg);
+			fgImg.copyTo(portraitPhotographTmp, notImg);
+			cv::imwrite("portraitPhotographTmp.jpg", portraitPhotographTmp);
+		}
+
+		// 背景画を抽出 
+		cv::Mat backgroundPictureTmp;
+		{
+			img_back.copyTo(backgroundPictureTmp, binaryImg);
+			cv::imwrite("backgroundPictureTmp.jpg", backgroundPictureTmp);
+		}
+
+		// 人物画と背景画を合成する。
+		cv::bitwise_or(backgroundPictureTmp, portraitPhotographTmp, result);
+		//cv::imshow("Show MASK COMPOSITION Image", result);
+	}
+#endif
 	cv::imwrite("result.jpg", result);
+
+#if false
+	cv::absdiff(result, bgImg, matDiff);
+	cv::cvtColor(matDiff, matDiff, cv::COLOR_BGR2GRAY);
+	cv::imwrite("matDiff2.png", matDiff);
+
+	cv::Mat binaryImg2(cv::Size(fgImg.cols, fgImg.rows), CV_8UC1, cv::Scalar::all(255));
+//	soft_mask(matDiff, binaryImg2, 20, 50);
+	cv::threshold(matDiff, binaryImg2, 10, 255, CV_THRESH_BINARY);
+//	cv::bitwise_not(binaryImg2, binaryImg2);
+	cv::imwrite("binary2.png", binaryImg2);
+#endif
+
+	{
+
+		// 入力画像の取得
+		cv::Mat im = result;
+		cv::Mat im2(im.size(), im.type());
+
+		// 画像配列を1次元に変換
+		cv::Mat points;
+		im.convertTo(points, CV_32FC3);
+		points = points.reshape(3, im.rows*im.cols);
+
+		// RGB空間でk-meansクラスタリングを実行
+		cv::Mat_<int> clusters(points.size(), CV_32SC1);
+		cv::Mat centers;
+
+		// クラスタ数
+		const int cluster = 5;
+		// k-meansnクラスタリングの実行
+		kmeans(points, cluster, clusters, cvTermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1.0), 1, cv::KMEANS_PP_CENTERS, centers);
+
+		// 画素値をクラスタ中心値に置換
+		cv::MatIterator_<cv::Vec3b>   itd = im2.begin<cv::Vec3b>();
+		cv::MatIterator_<cv::Vec3b>   itd_end = im2.end<cv::Vec3b>();
+		for (int i = 0; itd != itd_end; ++itd, ++i) {
+			cv::Vec3f &color = centers.at<cv::Vec3f>(clusters(i), 0);
+			(*itd)[0] = cv::saturate_cast<uchar>(color[0]);
+			(*itd)[1] = cv::saturate_cast<uchar>(color[1]);
+			(*itd)[2] = cv::saturate_cast<uchar>(color[2]);
+		}
+		// 結果表示
+		imshow("Input", im);
+		imshow("Output", im2);
+		cv::absdiff(im, im2, matDiff);
+		imshow("Output2", matDiff);
+	}
+
+	cv::Mat binaryImg3(cv::Size(fgImg.cols, fgImg.rows), CV_8UC1, cv::Scalar::all(255));
+	//	soft_mask(matDiff, binaryImg2, 20, 50);
+	cv::cvtColor(result, result, cv::COLOR_BGR2GRAY);
+	cv::imwrite("result_gray.png", result);
+	cv::threshold(result, binaryImg3, 40, 255, CV_THRESH_BINARY);
+	cv::imwrite("binary2.png", binaryImg3);
+
+	cv::imwrite("result2.jpg", result);
+	cv::waitKey();
+}
+
+//減色用の関数
+int convert(int value) {
+	if (value < 32) {
+		return 32;
+	}
+	else if (value < 64) {
+		return 64;
+	}
+	else if (value < 96) {
+		return 96;
+	}
+	else if (value < 128) {
+		return 128;
+	}
+	else if (value < 160) {
+		return 160;
+	}
+	else if (value < 192) {
+		return 192;
+	}
+	else if (value < 224) {
+		return 224;
+	}
+	else {
+		return 255;
+	}
+	return 0;  // 未到達
+}
+
+void sample7()
+{
+	using namespace cv;
+	using namespace std;
+
+	//画像の読み込みと表示
+	Mat input = imread("sample_fg.jpg", cv::IMREAD_COLOR);
+	if (input.empty()) {
+		cout << "Failed to load image!" << endl;
+		return;
+	}
+	imshow("original", input);
+
+	//正規化・減色
+	Mat norm(input.size(), input.type());
+	Mat sample(input.size(), input.type());
+	normalize(input, norm, 0, 255, NORM_MINMAX, CV_8UC3);
+	for (int y = 0; y<input.rows; y++) {
+		for (int x = 0; x<input.cols; x++) {
+			for (int c = 0; c < input.channels(); ++c) {
+				int index = y*input.step + x*input.elemSize() + c;
+				sample.data[index] = convert(norm.data[index]);
+			}
+		}
+	}
+	input = sample;
+	imshow("down", input);
+
+	//kmeansを行うためにフォーマットを変換
+	Mat matrix = input.reshape(1, input.cols*input.rows);
+	matrix.convertTo(matrix, CV_32FC1, 1.0 / 255.0);
+
+	//kmeansの実行
+	Mat centers, labels; //結果の格納
+	TermCriteria criteria(TermCriteria::COUNT, 100, 1); //100回繰り返す
+	int cluster_number = 6; //クラスタ数
+	kmeans(matrix, cluster_number, labels, criteria, 1, KMEANS_RANDOM_CENTERS, centers);
+
+	//レイヤーの準備(初期状態を白で統一)
+	Mat layer0(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+	Mat layer1(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+	Mat layer2(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+	Mat layer3(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+	Mat layer4(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+	Mat layer5(Size(input.cols, input.rows), CV_8UC3, Scalar::all(255));
+
+	//色とラベル情報の準備
+	MatConstIterator_<int> label_first = labels.begin<int>();
+	centers.convertTo(centers, CV_8UC1, 255.0);
+	centers = centers.reshape(3);
+
+	//レイヤーごとに色を分ける
+	for (int y = 0; y<input.rows; ++y) {
+		for (int x = 0; x<input.cols; ++x) {
+			const Vec3b& rgb = centers.ptr<Vec3b>(*label_first)[0];
+			if (*label_first == 0) layer0.at<Vec3b>(y, x) = rgb;
+			else if (*label_first == 1) layer1.at<Vec3b>(y, x) = rgb;
+			else if (*label_first == 2) layer2.at<Vec3b>(y, x) = rgb;
+			else if (*label_first == 3) layer3.at<Vec3b>(y, x) = rgb;
+			else if (*label_first == 4) layer4.at<Vec3b>(y, x) = rgb;
+			else layer5.at<Vec3b>(y, x) = rgb;
+			++label_first;
+		}
+	}
+	//レイヤーごとに表示
+	imshow("layer0", layer0);
+	imshow("layer1", layer1);
+	imshow("layer2", layer2);
+	imshow("layer3", layer3);
+	imshow("layer4", layer4);
+	imshow("layer5", layer5);
+
+	//レイヤーごとに保存
+	imwrite("layer0.png", layer0);
+	imwrite("layer1.png", layer1);
+	imwrite("layer2.png", layer2);
+	imwrite("layer3.png", layer3);
+	imwrite("layer4.png", layer4);
+	imwrite("layer5.png", layer5);
+
+	waitKey(0);
+}
+
+void sample8()
+{
+	using namespace cv;
+	using namespace std;
+
+	const char* input  = "sample_fg.jpg";
+	const char* mask   = "mask.png";
+	const char* output = "new.png";
+	const char* mask2  = "layer0.png";
+
+	cv::Mat image_src = cv::imread(input);
+	cv::Mat image_mask = cv::imread(mask, 0);
+	cv::Mat image_mask2 = cv::imread(mask2, 0);
+	cv::Mat image_dest;
+
+	cv::bitwise_not(image_mask, image_mask);
+	cv::bitwise_not(image_mask2, image_mask2);
+
+	// 素材画像をチャンネル(RGB)ごとに分離してvectorに格納する
+	std::vector<cv::Mat> mv;
+	cv::split(image_src, mv);
+
+	// 注目領域：マスク画像の中心の素材画像の大きさの領域
+	cv::Rect rect((image_mask2.cols - image_src.cols) / 2,
+		(image_mask2.rows - image_src.rows) / 2,
+		image_src.cols,
+		image_src.rows);
+	// vectorの最後尾にマスク画像の注目領域を追加する
+	mv.push_back(cv::Mat(image_mask2, rect));
+
+	// vectorを結合して加工後の画像とする
+	cv::merge(mv, image_dest);
+
+	// 加工後の画像を出力する
+	cv::imwrite(output, image_dest);
+
+
+	imshow("layer0", image_mask2);
+	waitKey(0);
 }
 
 int main(int argc, const char* argv[])
 {
 //	sample1();
-//	encode_decode();
 //	sample2();
 //	sample3();
 //	sample4();
-	sample6();
+//	sample6();
+//	sample7();
+	sample8();
 
 	return 0;
 }
